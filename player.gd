e49e8c1a59284a2e3620481e39faabf8e8ad08d6
AlaaHummada <alaahummada115@gmail.com>
2025-02-16 23:13:35 +0300

    Attacking

--- player.gd
+++ player.gd
@@ -6,7 +6,7 @@ const ROLL_SPEED = 500.0
 var is_rolling = false
 var direction: Vector2
 var roll_direction: Vector2  # <-- NEW: Store the roll direction
-
+var is_attacking = false
 func _physics_process(delta: float) -> void:
 	if is_rolling:
 		# Use the locked roll direction, not the updated "direction"
@@ -22,8 +22,11 @@ func _physics_process(delta: float) -> void:
 	if velocity.length() > 0.0:
 		$AnimatedSprite2D.play("RUN")
 		if velocity.x < 0:
+			$Sword.position.x=-50.0
 			$AnimatedSprite2D.flip_h = true
+			
 		elif velocity.x > 0:
+			$Sword.position.x=50.0
 			$AnimatedSprite2D.flip_h = false
 	else:
 		$AnimatedSprite2D.play("IDLE")
@@ -33,6 +36,20 @@ func _physics_process(delta: float) -> void:
 	# Start roll
 	if Input.is_action_just_pressed("roll") and $RollCooldown.is_stopped() and not is_rolling:
 		roll()
+		
+	if Input.is_action_just_pressed("attack") and $Sword.position.x<0 and not is_attacking:
+		$Sword/AnimationPlayer.play("Attack_Left")
+		is_attacking=true
+		await get_tree().create_timer($Sword/AnimationPlayer.current_animation_length).timeout
+		$Sword/AnimationPlayer.play("reset")
+		is_attacking=false
+		
+	if Input.is_action_just_pressed("attack") and $Sword.position.x>0 and not is_attacking:
+		$Sword/AnimationPlayer.play("Attack_Right")
+		is_attacking=true
+		await get_tree().create_timer($Sword/AnimationPlayer.current_animation_length).timeout
+		$Sword/AnimationPlayer.play("reset")
+		is_attacking=false
 
 func roll():
 	is_rolling = true
@@ -43,9 +60,9 @@ func roll():
 	roll_direction = direction.normalized()
 	if roll_direction == Vector2.ZERO:  # If no input, roll in facing direction
 		roll_direction = Vector2.RIGHT if !$AnimatedSprite2D.flip_h else Vector2.LEFT
-	
+	print($AnimatedSprite2D.animation)
 	# Wait for roll duration
-	await $RollTime.timeout
+	await $AnimatedSprite2D.animation_looped
 	
 	# Reset after roll
 	is_rolling = false
